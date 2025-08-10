7c6a045cea0b62e34b877555ef359092f458f92e
Matheus Pontes <matheuspontes5@hotmail.com>
2025-08-10 18:30:45 -0300

    agora vai

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,8 +1,4 @@
-// import a css file containig welcome styles
 import './Welcome.css'
-// Create a Welcome function returning the HTML of welcome block
-
-// Add a default export statement for Welcome component to use it in the other files
 
 function Welcome() {
     return (
